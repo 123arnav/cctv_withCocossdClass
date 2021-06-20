2cf70cc8e1674825339fb50ef0ff66cbd145aeda
123arnav <74090758+123arnav@users.noreply.github.com>
2021-06-20 11:31:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+var video="";
+var status="";
 function preload(){
-
+video=createVideo("video.mp4")
+video.hide();
 };
 
 function setup(){
-canvas=createCanvas(400,330)
-canvas.position(450,230)
+canvas=createCanvas(400,330);
+canvas.position(450,230);
+};
+function start(){
+objectDetector=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("status").innerHTML="Status:detecting";
 };
 
-function draw(){
+function modelloaded(){
+    console.log("loaded");
+    status="true";
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
 
+function draw(){
+image(video,0,0,400,330)
 };
