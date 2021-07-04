9d712ffdbb4658151a386b393faeeea7c72bfe1e
123arnav <74090758+123arnav@users.noreply.github.com>
2021-07-04 11:48:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var video="";
 var status="";
+var object=[];
 function preload(){
 video=createVideo("video.mp4")
 video.hide();
@@ -24,4 +25,30 @@ function modelloaded(){
 
 function draw(){
 image(video,0,0,400,330)
+if (status!=""){
+objectDetector.detect(video,gotresult)
+for(i=0; i<object.length; i++){
+    document.getElementById("status").innerHTML="Status:detected";
+    document.getElementById("objects").innerHTML=object.length;
+    confidence=Math.floor(object[i].confidence*100)+"%";
+    r=0;
+    g=Math.floor(Math.random()*255);
+    b=0;
+    fill(r,g,b)
+    text(object[i].label+"  "+confidence, object[i].x, object[i].y)
+    noFill();
+    stroke(r,g,b);
+    rect(object[i].x, object[i].y, object[i].width, object[i].height)
+    
+}
+};
+};
+function gotresult(error,result){
+    if (error){
+console.log(error);
+    }
+    else {
+        console.log(result);
+        object=result;
+    }
 };
